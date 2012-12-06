cc54e428354644361ac66715f8a4b27b2b606ea9
Darrow Cole <darrowco@library.ucla.edu>
2012-12-06 15:36:09 -0800

    no nowdate

--- date_test.php
+++ date_test.php
@@ -1,23 +1,3 @@
 <?php
 $nowDate = new DateTime("now");
-$d1=new DateTime("2012-07-08 11:14:15.638276");
-$d2=new DateTime("2012-07-08 11:14:15.889342");
-$diff=$d2->diff($d1);
-print_r( $diff ) ;
-
-/* returns:
-
-DateInterval Object
-(
-    [y] => 0
-    [m] => 0
-    [d] => 0
-    [h] => 0
-    [i] => 0
-    [s] => 0
-    [invert] => 0
-    [days] => 0
-)
-
-*/
 ?>
